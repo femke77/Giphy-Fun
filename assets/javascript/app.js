ccd809e1b578e21d7097230ae109b43c97920d5e
Meg Meyers <megan.g.meyers@gmail.com>
2019-11-12 23:11:48 -0800

    buttons working, ajax working, display gifs working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,58 @@
-var queryURL = "http://api.giphy.com/v1/gifs/q=" + value + "api_key=Rti16DDOi8KwkyUPSv7rAJca8jFi3Uve";
-value = "";
+$(document).ready(function () {
+
+    var topics = ["pizza", "burgers"];
+    
+    
+
+
+    for(var i = 0; i < topics.length; i++){
+        $(".buttons").append(
+            $("<button/>", {
+                text: topics[i],
+                click: getImages
+
+            })
+        )
+    }
+
+
+    function getImages(){
+        $(".images").empty();
+        searchVal = $(this).text();
+        console.log(searchVal);
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + 
+            searchVal + "&api_key=Rti16DDOi8KwkyUPSv7rAJca8jFi3Uve&limit=10";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function(response){
+                var results = response.data;
+                console.log(results);
+                for(var i = 0; i < results.length; i++){
+                    if (results[i].rating !== "r"){
+                        var imgDiv = $("<div>");
+                        imgDiv.addClass("float");
+                        var rating = results[i].rating;
+                        var p = $("<p>").text("Rating: " + rating);
+                        var foodImg = $("<img>");
+                        foodImg.attr("src", results[i].images.fixed_height_still.url);
+                        imgDiv.append(p);
+                        imgDiv.append(foodImg);
+                        $(".images").append(imgDiv);
+                    }
+                }
+
+            });
+    }
+
+
+
+
+
+
+
+
+});
 
