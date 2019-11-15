ff4232387bffaaa612af1f0bd84a6e594fa4ea65
Meg Meyers <megan.g.meyers@gmail.com>
2019-11-14 23:05:56 -0800

    added click animation and user request

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,84 @@
+//TODO 
+//DRY!!
+//style nicely
+//readme.md
+//do bonus content
+
 $(document).ready(function () {
 
-    var topics = ["pizza", "burgers"];
-
+    var topics = ["pizza", "burgers", "ice cream", "cake", "cupcakes", "muffins", "cereal", "pasta", "cookies", "steak"];
     
-    
-
 
     for(var i = 0; i < topics.length; i++){
         $(".buttons").append(
             $("<button/>", {
                 text: topics[i],
+                val: topics[i],
                 click: getImages
-
             })
-        )
+        );
     }
 
-
     function getImages(){
-        $(".images").empty();
-        searchVal = $(this).text();
-        console.log(searchVal);
+        $(".image-display").empty(); 
+        var searchVal = $(this).val();     
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + 
+            searchVal + "&api_key=Rti16DDOi8KwkyUPSv7rAJca8jFi3Uve&limit=10";
+        
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function(response){
+                 displayImages(response)
+            }
+               
+            );
+    }
+
+   function displayImages(response){
+    var results = response.data;
+    for(var i = 0; i < results.length; i++){
+        if (results[i].rating !== "r"){                       
+            var imgDiv = $("<div>");
+            imgDiv.addClass("float");
+            var rating = results[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
+            var foodImg = $("<img>");
+            foodImg.attr({
+                src: results[i].images.fixed_height_still.url,
+                alt: "food image",
+                "data-still": results[i].images.fixed_height_still.url,
+                "data-state": "still",
+                "data-animate": results[i].images.fixed_height.url
+            });                     
+            foodImg.addClass("gif");
+            imgDiv.append(p, foodImg);
+            $(".image-display").append(imgDiv);
+        }
+    }
+}
+
+
+    $("body").on("click", ".gif", function() {
+        var state = $(this).attr("data-state");
+        if (state === "still"){
+            $(this).attr({
+                src: $(this).attr("data-animate"),
+                "data-state": "animate"
+            });
+        } else {          
+            $(this).attr({
+                src: $(this).attr("data-still"),
+                "data-state": "still"
+            });
+        }
+    });
+
+    $(".btn").on("click", function(e) {
+        e.preventDefault();
+        $(".image-display").empty();
+        var searchVal = $("#user-request").val();
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + 
             searchVal + "&api_key=Rti16DDOi8KwkyUPSv7rAJca8jFi3Uve&limit=10";
 
@@ -29,38 +87,11 @@ $(document).ready(function () {
             method: "GET"
         })
             .then(function(response){
-                var results = response.data;
-                console.log(results);
-                for(var i = 0; i < results.length; i++){
-                    if (results[i].rating !== "r"){                       
-                        var imgDiv = $("<div>");
-                        imgDiv.addClass("float");
-                        var rating = results[i].rating;
-                        var p = $("<p>").text("Rating: " + rating);
-                        var foodImg = $("<img>");
-                        foodImg.attr("src", results[i].images.fixed_height_still.url);
-                        foodImg.attr("alt", "food image");
-                        foodImg.attr("data-still" , results[i].images.fixed_height_still.url)
-                        foodImg.attr("data-animate", results[i].images.fixed_height.url)
-                        foodImg.attr("data-state", "still")
-                        foodImg.addClass("gif");
-                        imgDiv.append(p);
-                        imgDiv.append(foodImg);
-                        $(".image-display").append(imgDiv);
-                    }
+                displayImages(response)
+                
                 }
-
-            });
-    }
-
-
-
-    $("body").on("click", ".gif", function() {
-   
-        var state = $(this).attr("data-state");
-        console.log(state);
-
-    });
+            );
+    })
 
 
 });
